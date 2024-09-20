2395cb9dba8db5d59d1624e3d3fb0ba3e455e549
iasmim souza <165791722+mimisouzaa@users.noreply.github.com>
2024-09-20 09:55:13 -0300

    Add files via upload

--- ATIVIDADE CISCO/script.js
+++ ATIVIDADE CISCO/script.js
@@ -1,9 +1,10 @@
-function calcular(){
-    var val1 = parseFloat (document.getElementById('val1').value);
-    var val2 = parseFloat (document.getElementById('val1').value);
-    var val3 = parseFloat (document.getElementById ('val1').value);
-
-    var media = (val1 + val2 + val3) / 3;
-    document.getElementById ('resultado').innerText = media;
-
-}
+ function cisco(){
+    const k =8.9875 * Math.pow(10, 9);
+    const Q1 = parseFloat(document.getElementById('Q1').value);
+    const Q2 = parseFloat(document.getElementById('q2').value);
+    const D = parseFloat(document.getElementById('D').value);
+    
+     const result = k * Math.abs (Q1*Q2) / Math.pow (D, 2);
+     document.getElementById('result').innerText = result.toExponential(2);
+     }
+ 
